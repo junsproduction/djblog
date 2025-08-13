8489f8f5e24be1b8e2b6e466313bf813dedf3ac0
junsproduction <junsproduction1@gmail.com>
2025-08-13 20:06:23 +0700

    fixed directory output

--- djblogsite/settings.py
+++ djblogsite/settings.py
@@ -176,7 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Changed from 'staticfiles' to 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Changed back to 'staticfiles'
 STATICFILES_DIRS = []
 
 # Media files configuration
